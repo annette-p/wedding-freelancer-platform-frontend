76e6e43ffd472ce5b81f54491126dadbc00a5512
Annette Poh <coding.net.p@gmail.com>
2021-10-08 19:04:13 +0800

    navbar and react bootstrap installation

--- src/WeddingFreelancerPlatform.js
+++ src/WeddingFreelancerPlatform.js
@@ -1,5 +1,11 @@
 import React from "react"
-// import Navbar from "react-bootstrap/Navbar"
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "react-bootstrap/Navbar"
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
 
 export default class WeddingFreelancerPlatform extends React.Component {
     state = {
@@ -9,54 +15,38 @@ export default class WeddingFreelancerPlatform extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <nav class="navbar navbar-light bg-light">
-                    <div class="container-fluid">
-                        <button class="navbar-brand">Navbar</button>
-                        <div class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                        </div>
-                    </div>
-                </nav>
-
-
-
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <div className="container-fluid">
-                        <button className="navbar-brand">Navbar</button>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" >Home</a>
-                            </li>
-                            <li className="nav-item">
-                            <a className="nav-link">Link</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a className="dropdown-item">Action</a></li>
-                                <li><a className="dropdown-item">Another action</a></li>
-                                <li><hr className="dropdown-divider"/></li>
-                                <li><a className="dropdown-item">Something else here</a></li>
-                            </ul>
-                            </li>
-                            <li className="nav-item">
-                            <a className="nav-link disabled">Disabled</a>
-                            </li>
-                        </ul>
-                        <div className="d-flex">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                            <button className="btn btn-outline-success" type="submit">Search</button>
-                        </div>
-                        </div>
-                    </div>
-                </nav>
+                <Navbar bg="light" expand="lg">
+                    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                        className="mr-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                        >
+                        <Nav.Link href="#action1">Home</Nav.Link>
+                        <Nav.Link href="#action2">Link</Nav.Link>
+                        <NavDropdown title="Link" id="navbarScrollingDropdown">
+                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#" disabled>
+                            Link
+                        </Nav.Link>
+                        </Nav>
+                        <Form className="d-flex">
+                        <FormControl
+                            type="search"
+                            placeholder="Search"
+                            className="mr-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </Navbar>
             </React.Fragment>
         )
     }
