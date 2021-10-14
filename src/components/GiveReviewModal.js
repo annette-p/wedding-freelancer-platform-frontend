b144e04c94175222e6fb11038c8df4daa79f2941
Annette Poh <coding.net.p@gmail.com>
2021-10-14 15:53:11 +0800

    completed styling for review submission form

--- src/components/GiveReviewModal.js
+++ src/components/GiveReviewModal.js
@@ -14,7 +14,7 @@ export default function GiveReviewModal(props) {
                 <div className="modal-dialog modal-lg">
                     <div className="modal-content">
                         <div className="modal-header">
-                        <h3>Help other customer know about the services you just received</h3>
+                        <h4>Help other customer know about the services you just received</h4>
                         <button
                             type="button"
                             className="btn-close"
@@ -25,26 +25,29 @@ export default function GiveReviewModal(props) {
                         </div>
                         <div className="modal-body">
                             <div className="row">
-                                <h5 className="modal-title">Review and Rate {props.freelancer.name}</h5>
-                                <label className="form-label">Your Name: </label>
+                                <h5 className="modal-title">Rating and Review for {props.freelancer.name}</h5>
+                                <label className="form-label mt-3">Your Name: </label>
                                 <input type="text" name="" value="" onChange="" className="form-control"/>
-                                <label className="form-label">Your Email (optional): </label>
+                                <label className="form-label mt-3">Your Email (optional): </label>
                                 <input type="text" name="" value="" onChange="" className="form-control"/>
                             </div>
                             {/* <hr></hr> */}
                             <div className="row">
-                                <label className="mb-2"> Rate {props.freelancer.name} from 1(bad) to 5(excellent)</label>
-                                <input type="radio" name="rate" value="one" /><span>1</span>
-                                <input type="radio" name="rate" value="two" /><span>2</span>
-                                <input type="radio" name="rate" value="three" /><span>3</span>
-                                <input type="radio" name="rate" value="four" /><span>4</span>
-                                <input type="radio" name="rate" value="five" /><span>5</span>
-                                <label className="form-label">Comment: </label>
-                                <input type="text" name="" value="" onChange="" className="form-control"/>
+                                <label className="mt-3 my-2"> Rate {props.freelancer.name} from: 1(bad) to 5(excellent)</label>
+                                <div><input type="radio" name="rate" value="one" /><span className="ms-2">1</span></div>
+                                <div><input type="radio" name="rate" value="two" /><span className="ms-2">2</span></div>
+                                <div><input type="radio" name="rate" value="three" /><span className="ms-2">3</span></div>
+                                <div><input type="radio" name="rate" value="four" /><span className="ms-2">4</span></div>
+                                <div><input type="radio" name="rate" value="five" /><span className="ms-2">5</span></div>
+                                <label className="mt-3 my-2"> Will you recommend {props.freelancer.name} to your friend and family?</label>
+                                <div><input type="radio" name="recommend" value="yes" /><span className="ms-2">Yes</span></div>
+                                <div><input type="radio" name="recommend" value="no" /><span className="ms-2">No</span></div>
+                                <label className="form-label mt-3">Comment: </label>
+                                <input type="text" name="" value="" onChange="" className="form-control comment-box"/>
                             </div>
                         </div>
                         <div className="modal-footer">
-                            <button type="button" className="btn btn-primary" onClick={props.hideModal}>Save changes</button>
+                            <button type="button" className="btn btn-primary" onClick={props.hideModal}>Submit and Close</button>
                         </div>
                     </div>
                 </div>
