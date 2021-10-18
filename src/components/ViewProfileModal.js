aa1989075941a5ea97d215e6df4d00a7ef7f7056
Annette Poh <coding.net.p@gmail.com>
2021-10-18 23:20:36 +0800

    change to use react bootstap Carousel to fix the automatic sliding issue

--- src/components/ViewProfileModal.js
+++ src/components/ViewProfileModal.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
-// import Carousel from 'react-bootstrap/Carousel'
+import Carousel from 'react-bootstrap/Carousel'
 
 export default function ViewProfileModal(props) {
   return (
@@ -48,7 +48,10 @@ export default function ViewProfileModal(props) {
                         {/* Carousel */}
                         <div className="row">
                             <h5>Portfolio</h5>
-                            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+
+                            {/* to remove */}
+                            <div>
+                            {/* <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
                                 <div class="carousel-indicators">
                                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -85,51 +88,49 @@ export default function ViewProfileModal(props) {
                                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                     <span class="visually-hidden">Next</span>
                                 </button>
+                            </div> */}
                             </div>
+                            {/* end of to remove */}
                             
-                            {/* to remove */}
-                            <div>
-                            {/* <Carousel>
-                            <Carousel.Item>
-                                <img
-                                className="d-block w-100"
-                                src={require('../images/portfolio/museum.jpg').default}
-                                // src="holder.js/800x400?text=First slide&bg=373940"
-                                alt="First slide"
-                                />
-                                <Carousel.Caption>
-                                <h3>First slide label</h3>
-                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <img
-                                className="d-block w-100"
-                                src={require('../images/portfolio/lake_gardens.jpg').default}
-                                // src="holder.js/800x400?text=Second slide&bg=282c34"
-                                alt="Second slide"
-                                />
-
-                                <Carousel.Caption>
-                                <h3>Second slide label</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <img
-                                className="d-block w-100"
-                                src={require('../images/portfolio/raffles_hotel.jpg').default}
-                                // src="holder.js/800x400?text=Third slide&bg=20232a"
-                                alt="Third slide"
-                                />
-
-                                <Carousel.Caption>
-                                <h3>Third slide label</h3>
-                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                            </Carousel> */}
-                            </div>
+                            <Carousel>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-100"
+                                        src={require('../images/portfolio/museum.jpg').default}
+                                        // src="holder.js/800x400?text=First slide&bg=373940"
+                                        alt="First slide"
+                                    />
+                                    <Carousel.Caption>
+                                        <h3>First slide label</h3>
+                                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-100"
+                                        src={require('../images/portfolio/lake_gardens.jpg').default}
+                                        // src="holder.js/800x400?text=Second slide&bg=282c34"
+                                        alt="Second slide"
+                                    />
+                                    <Carousel.Caption>
+                                        <h3>Second slide label</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-100"
+                                        src={require('../images/portfolio/raffles_hotel.jpg').default}
+                                        // src="holder.js/800x400?text=Third slide&bg=20232a"
+                                        alt="Third slide"
+                                    />
+                                    <Carousel.Caption>
+                                        <h3>Third slide label</h3>
+                                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+                            </Carousel>
+                            
 
                         </div>
                         <hr></hr>
