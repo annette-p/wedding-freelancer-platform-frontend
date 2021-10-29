4ca19f311b46c995c83ebe5ffed3ad1fdc9908f1
Annette Poh <coding.net.p@gmail.com>
2021-10-30 00:00:12 +0800

    set each device width responsiveness styling for contact & social media details section in manage profile page

--- src/components/EditProfileForm.js
+++ src/components/EditProfileForm.js
@@ -199,36 +199,37 @@ export default class EditProfileForm extends React.Component {
         if (this.state.activeDisplay === "social-media") {
             return (
                 <div className="row register-text">
-                    <h3 className="mt-4 account-form">Contact &amp; Social Media Setting</h3>
+                    <h3 className="account-form d-none d-md-block">Contact &amp; Social Media Setting</h3>
+                    <h4 className="account-form d-md-none">Contact &amp; Social Media Setting</h4>
                     {/* Social Media */}                   
-                    <div className="col mt-4 mb-4">
+                    <div className="col-sm-12 col-md mt-4 mb-md-4">
                         <label className="form-label register-form-headline head-center mt-4">Social Media:</label>
-                        <div className="mt-3">
+                        <div className="mt-sm-3 mt-md-4">
                             <label className="form-label">Facebook:</label>
                             <input type="text" name="facebook" value={this.state.facebook} onChange={this.updateFormField} placeholder="Facebook URL" className="form-control"/>
                         </div>
-                        <div className="mt-4">
+                        <div className="mt-sm-3 mt-md-4">
                             <label className="form-label">Instagram:</label>
                             <input type="text" name="instagram" value={this.state.instagram} onChange={this.updateFormField} placeholder="Instagram URL" className="form-control"/>
                         </div>
-                        <div className="mt-4">
+                        <div className="mt-sm-3 mt-md-4">
                             <label className="form-label">Tiktok:</label>
                             <input type="text" name="tiktok" value={this.state.tiktok} onChange={this.updateFormField} placeholder="Tiktok URL" className="form-control"/>
                         </div>
                         <div className="error-msg">{this.state.errors.socialMedia}</div>
                     </div>
                     {/* Contact */}
-                    <div className="col mt-4 mb-4">
+                    <div className="col-sm-12 col-md mt-4 mb-4">
                         <label className="form-label register-form-headline head-center mt-4">Contact:</label>
-                        <div className="mt-3">
+                        <div className="mt-sm-3 mt-md-4">
                             <label className="form-label">Mobile:</label>
                             <input type="text" name="mobile" value={this.state.mobile} onChange={this.updateFormField} placeholder="mobile number" className="form-control"/>
                         </div>
-                        <div className="mt-4">
+                        <div className="mt-sm-3 mt-md-4">
                             <label className="form-label">Email:</label>
                             <input type="text" name="email" value={this.state.email} onChange={this.updateFormField} placeholder="email address" className="form-control"/>
                         </div>
-                        <div className="mt-4">
+                        <div className="mt-sm-3 mt-md-4">
                             <label className="form-label">Website:</label>
                             <input type="text" name="web" value={this.state.website} onChange={this.updateFormField} placeholder="website URL" className="form-control"/>
                         </div>
