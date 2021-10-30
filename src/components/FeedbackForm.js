961d2b2bac6f8db64e0f96f9810ad2fc55358248
Annette Poh <coding.net.p@gmail.com>
2021-10-30 19:13:01 +0800

    set device responsiveness for each screen size for feedback form

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -30,32 +30,37 @@ export default class FeedbackForm extends React.Component {
         return (
             <React.Fragment>
                 <div className="row mt-4">
-                    <h3 className="d-none d-md-block login-title">Your Feedback is valuable to us</h3>
-                    <h5 className="d-md-none login-title">Your Feedback is valuable to us</h5>
-                    <div className="row">
-                        <div className="col-2"></div>
-                        <div className="col-sm-12 col-md-8">
-                            <label className="form-label register-form-headline">Name:</label>
+                    <h3 className="d-none d-md-block feedback-title">Your Feedback is valuable to us</h3>
+                    <h5 className="d-md-none feedback-title">Your Feedback is valuable to us</h5>
+                    <div className="row feedback-form ms-md-2">
+                        <div className="d-none d-lg-block col-2"></div>
+                        <div className="col col-lg-8">
+                            <label className="form-label register-form-headline mt-4">Name:</label>
                             <input type="text" name="name" value={this.state.name} onChange={this.updateFormField} className="form-control"/>
 
-                            <label className="form-label register-form-headline">Email: </label>
-                            <input type="text" name="email" value={this.state.email} onChange={this.updateForm} className="form-control"/>
+                            <label className="form-label register-form-headline mt-4">Email: </label>
+                            <input type="text" name="email" value={this.state.email} onChange={this.updateFormField} className="form-control"/>
 
-                            <label className="form-label register-form-headline">Your Feedback / Questions:</label>
+                            <label className="form-label register-form-headline mt-4">Your Feedback / Questions:</label>
                             <input type="text" name="feedback" value={this.state.feedback} onChange={this.updateFormField} placeholder="Type your feedback or question to us" className="form-control bio-box"/>
 
-                            <button 
-                                onClick={this.addFeedback}
-                                className="btn btn-secondary btn-lg account-btn" 
-                                type="button">
-                                 Send Your Message
-                            </button>
+                            <div className="account-creation-form feedback-btn mt-md-4">
+                                <div className="d-grid gap-2 mb-2">
+                                    <button 
+                                        onClick={this.addFeedback}
+                                        className="btn btn-secondary btn-lg account-btn font-size-btn" 
+                                        type="button">
+                                        Send Your Message
+                                    </button>
+                                </div>
+                            </div>
                         </div>
-                        <div className="col-2"></div>
+                        <div className="d-none d-lg-block col-2"></div>
+                        <div className="d-block d-md-none d-lg-none" style={{height: "15px"}}></div>
+                        <div className="d-none d-md-block d-lg-none" style={{height: "335px"}}></div>
+                        <div className="d-none d-lg-block" style={{height: "180px"}}></div>
                     </div>
                 </div>
-                
-                
             </React.Fragment>
         )
     }
